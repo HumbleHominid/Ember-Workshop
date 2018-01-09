45742cd4159282c6ef0a3a37dcccd483251d7e92
Michael Fryer <michael.d.fryer@gmail.com>
2018-01-09 11:52:27 -0800

    refactored post-list/item to only have one modal rather than N modals

--- app/components/post-list.js
+++ app/components/post-list.js
@@ -1,5 +1,16 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: [ "card-columns" ]
+  classNames: [ "card-columns" ],
+
+  modalInfo: null,
+
+  actions: {
+    updateModalInfo(info) {
+      this.set('modalInfo', info);
+    },
+    nullifyModalInfo() {
+      this.set('modalInfo', null);
+    }
+  }
 });
